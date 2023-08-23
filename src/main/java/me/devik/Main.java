26b328f5bece16fabc2d95553f0605f4d08f5e08
JMaslo <hry.v@post.cz>
2023-08-23 16:50:10 +0200

    Created a method

--- src/main/java/me/devik/Main.java
+++ src/main/java/me/devik/Main.java
@@ -1,5 +1,23 @@
 package me.devik;
+
+import me.devik.systemManager.SystemManager;
+
+import java.util.Scanner;
+
 public class Main {
+    public static SystemManager manager = new SystemManager();
     public static void main(String[] args) {
+
+
+
+        System.out.println("-----------------------");
+        System.out.println("Welcome in our Reservation System");
+        System.out.println("We would be happy if you will continue :)");
+        System.out.println("-----------------------");
+
+        manager.chooseRoom();
     }
+
+
+
 }
--- src/main/java/me/devik/systemManager/SystemManager.java
+++ src/main/java/me/devik/systemManager/SystemManager.java
@@ -1,4 +1,19 @@
 package me.devik.systemManager;
 
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class SystemManager {
+
+    public void chooseRoom() {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter the amount of people: ");
+        int people = sc.nextInt(); // Save the number of people
+
+        System.out.println("Enter the amount of night: ");
+        int nights = sc.nextInt(); // Save the amount of nights
+
+    }
 }
